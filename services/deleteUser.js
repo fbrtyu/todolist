2dfbc509206f0c6be28eb498ccc0f26f89de50b1
Ilya <azazafkz@gmail.com>
2024-12-18 13:28:36 +0300

    Не работает onDelete cascade

--- services/deleteUser.js
+++ services/deleteUser.js
@@ -9,23 +9,30 @@ let deleteUser = async (login, password) => {
         if (
           await bcrypt.compare(password, currentUser[0].dataValues.password)
         ) {
-          auth = await db.db.Auth.destroy({
-            where: {
-              login: login,
-            },
-          })
-          taskOfUser = await db.db.TaskOfUser.destroy({
-            where: { idUser: currentUser[0].dataValues.id },
-          })
-          user = await db.db.User.destroy({
-            where: { id: currentUser[0].dataValues.id },
-          })
-          console.log(currentUser[0].dataValues.id)
-          console.log(await db.db.Auth.findAll())
-          console.log(await db.db.User.findAll())
-          console.log(await db.db.Task.findAll())
-          console.log(await db.db.TaskOfUser.findAll())
-          return 200
+          try {
+            // onDelete cascade, но почему то это не работает, 
+            // при удалении родительской записи, зависимая не удаляется
+            auth = await db.db.Auth.destroy({
+              where: { idUser: currentUser[0].dataValues.idUser },
+            })
+
+            taskuser = await db.db.TaskOfUser.destroy({
+              where: { idUser: currentUser[0].dataValues.idUser },
+            })
+
+            user = await db.db.User.destroy({
+              where: { id: currentUser[0].dataValues.idUser },
+            })
+
+            console.log(await db.db.Auth.findAll())
+            console.log(await db.db.User.findAll())
+            console.log(await db.db.Task.findAll())
+            console.log(await db.db.TaskOfUser.findAll())
+
+            return 200
+          } catch (err) {
+            console.log(err)
+          }
         } else {
           return 500
         }
